72bc097205ca68c055e1f86504ec89cbf1c6ac4d
Ditthapeem <lakagul.peem@gmail.com>
2024-06-13 21:46:48 +0700

    Adding a cache decorators

--- nutrition_service_api/urls.py
+++ nutrition_service_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
+    path('nutrition_service_api/api/', views.getRoutes, name="routes"),
     path('nutrition_service_api/api/get_all_food', views.get_all_food)
 ]
